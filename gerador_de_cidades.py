d88fc6a786483ffc44c14be1f506edc5efdd26ad
Leticia Torres <leticiatp2@gmail.com>
2025-02-28 14:28:31 -0300

    gerador alterado

--- gerador_de_cidades.py
+++ gerador_de_cidades.py
@@ -1,13 +1,38 @@
 import random
+import math
 
 class Gerador_de_Cidades():
 
     def __init__(self, tamanho):
         self.tam = tamanho
+        self.cidades = []
         self.m_pesos = []
+
+    def gerar_pontos(self):
+
+        cid_anterior = (0, 0)
+        tamanho = self.tam
+
+        while tamanho != 0:
+
+            x = random.randint(1, 40)
+            y = random.randint(1, 40)
+
+            cid = (x, y)
+
+            if(cid != cid_anterior):
+                self.cidades.append(cid)
+                cid_anterior = cid
+                tamanho -= 1
+            
+        print(self.cidades)
+
+    def calcula_dist(self, p1, p2):
+        return math.sqrt(math.pow((p2[0]-p1[0]), 2) + math.pow((p2[1]-p1[1]), 2))
 
     def gerar_matriz_de_adj(self):
         self.m_pesos = [[0]*self.tam for _ in range(self.tam)]
+        self.gerar_pontos()
 
         for i in range(self.tam):
             for j in range(self.tam):
@@ -19,16 +44,11 @@ class Gerador_de_Cidades():
                     self.m_pesos[i][j] = self.m_pesos[j][i]
 
                 if i != j and self.m_pesos[i][j] == 0:
-                    num_aleatorio = random.randint(0, 50)
+                    dist = self.calcula_dist(self.cidades[i], self.cidades[j])
 
-                    if num_aleatorio == 0:
-                        num_aleatorio = -1
-                    else: 
-                        pass
-
-                    self.m_pesos[i][j] = num_aleatorio
+                    self.m_pesos[i][j] = dist
                 
-        print(self.m_pesos)
+        print('Matriz finalizada!')
 
     def salvar_matriz(self):
         nome = 'matriz' + str(self.tam) + '.txt'
@@ -44,6 +64,9 @@ if __name__ == '__main__':
         gerador = Gerador_de_Cidades(i)
         gerador.gerar_matriz_de_adj()
         gerador.salvar_matriz()
+
+
+
 
 
 
